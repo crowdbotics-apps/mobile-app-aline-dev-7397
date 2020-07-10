f253cf612b8c3425bc0da0ce9e0789e585ca9369
Crowdbotics <team@crowdbotics.com>
2020-07-10 19:42:52 +0000

    Update components

--- src/features/BlankScreen18754/screens/blank.js
+++ src/features/BlankScreen18754/screens/blank.js
@@ -159,6 +159,36 @@ export class _Blank extends React.Component {
           >
             Sample text content
           </Text>
+          <Text
+            style={{
+              marginLeft: 0,
+              marginRight: 0,
+              marginTop: 5,
+              marginBottom: 5,
+              paddingLeft: 5,
+              paddingRight: 5,
+              paddingTop: 5,
+              paddingBottom: 5,
+              overflow: "visible",
+              textAlign: "left",
+              verticalAlign: "baseline",
+              fontSize: 20,
+              color: "#000000",
+              backgroundColor: "#ffffff",
+              fontStyle: "normal",
+              fontWeight: "normal",
+              borderColor: "#000000",
+              borderStyle: "solid",
+              borderWidth: 0,
+              borderLeftWidth: 0,
+              borderRightWidth: 0,
+              borderTopWidth: 0,
+              borderBottomWidth: 0,
+              borderRadius: 0
+            }}
+          >
+            My other text Element
+          </Text>
         </View>
         <View
           style={{
